0c999bc16bb6816631b3514e38bf48a94f07fd0b
Divyansh Sinha <114439802+CaptainDevon@users.noreply.github.com>
2024-06-22 02:52:01 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
            <h1 className="text-4x sm:text-6xl lg:text-7xl text-center tracking-wide">
                 Virtual build tools  
                 
-                <span className='bg-gradient-to-r from-orange-500 to red-800 text-transparent bg-clip-text'>{" "}for developers</span>
+                <span className='bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text'>{" "}for developers</span>
             </h1> 
             <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
                Empower your creativity and bring your VR app ideas to life with our
